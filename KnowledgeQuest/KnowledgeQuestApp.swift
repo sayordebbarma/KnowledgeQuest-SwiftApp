1dca5bb974e8412c8d4acef93aaab6e0fb381e1a
sayordebbarma <sayor.debbarma2021@vitbhopal.ac.in>
2023-02-22 21:11:46 +0530

    Initial Commit

---
+++ KnowledgeQuest/KnowledgeQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  KnowledgeQuestApp.swift
+//  KnowledgeQuest
+//
+//  Created by Sayor Debbarma on 22/02/23.
+//
+
+import SwiftUI
+
+@main
+struct KnowledgeQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
